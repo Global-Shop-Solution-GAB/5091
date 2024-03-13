1b1d170974c15a47ca555e2895296b2d09613faf
Service Web <sw@gssmail.com>
2024-03-13 11:06:45 -0500

    Commit changes in C:\Websites\Git\emarlow\5091\5091.sql

---
+++ 5091.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5091_SalesPerson
+ * ========================================== */
+CREATE TABLE "GCG_5091_SalesPerson"(
+ "GSS_Codes" CHAR(3),
+ "Salesman_ID" CHAR(7));
+
+
